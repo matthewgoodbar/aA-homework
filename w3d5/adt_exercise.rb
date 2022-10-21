a21d963c74de64aef32b8f3ef67f6a1a3cca5f81
matthew <matthewgoodbar@gmail.com>
2022-10-20 20:20:35 -0700

    Implement queue adt

--- w3d5/adt_exercise.rb
+++ w3d5/adt_exercise.rb
@@ -15,3 +15,21 @@ class Stack
         @arr[-1]
     end
 end
+
+class Queue
+    def initialize
+        @arr = []
+    end
+
+    def enqueue(el)
+        @arr.push(el)
+    end
+
+    def dequeue
+        @arr.shift
+    end
+
+    def peek
+        @arr[0]
+    end
+end
