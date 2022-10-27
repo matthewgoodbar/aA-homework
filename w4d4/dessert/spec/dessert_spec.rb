e0fe366174d9b6e53a8935ee5e43b0675f556417
matthew <matthewgoodbar@gmail.com>
2022-10-26 20:44:24 -0700

    implement dessert specs

--- w4d4/dessert/spec/dessert_spec.rb
+++ w4d4/dessert/spec/dessert_spec.rb
@@ -7,19 +7,30 @@ Instructions: implement all of the pending specs (the `it` statements without bl
 
 describe Dessert do
   let(:chef) { double("chef") }
+  subject(:dessert) { Dessert.new("pie", 3, chef) }
 
   describe "#initialize" do
-    it "sets a type"
-
-    it "sets a quantity"
-
-    it "starts ingredients as an empty array"
-
-    it "raises an argument error when given a non-integer quantity"
+    it "sets a type" do
+      expect(dessert.type).to eq("pie")
+    end
+    it "sets a quantity" do
+      expect(dessert.quantity).to eq(3)
+    end
+    it "starts ingredients as an empty array" do
+      expect(dessert.ingredients).to eq([])
+    end
+    it "raises an argument error when given a non-integer quantity" do
+      expect { Dessert.new("pie", "hello", chef) }.to raise_error(ArgumentError)
+    end
   end
 
   describe "#add_ingredient" do
-    it "adds an ingredient to the ingredients array"
+    before(:each) do
+      dessert.add_ingredient("ham")
+    end
+    it "adds an ingredient to the ingredients array" do
+      expect(dessert.ingredients).to include("ham")
+    end
   end
 
   describe "#mix!" do
@@ -27,13 +38,22 @@ describe Dessert do
   end
 
   describe "#eat" do
-    it "subtracts an amount from the quantity"
-
-    it "raises an error if the amount is greater than the quantity"
+    before(:each) do
+      dessert.eat(2)
+    end
+    it "subtracts an amount from the quantity" do
+      expect(dessert.quantity).to eq(1)
+    end
+    it "raises an error if the amount is greater than the quantity" do
+      expect { dessert.eat(4) }.to raise_error(RuntimeError)
+    end
   end
 
   describe "#serve" do
-    it "contains the titleized version of the chef's name"
+  it "contains the titleized version of the chef's name" do
+      allow(:chef).to receive(:titleize).and_return("Chef")
+      expect(dessert.serve).to include("Chef")
+    end
   end
 
   describe "#make_more" do
